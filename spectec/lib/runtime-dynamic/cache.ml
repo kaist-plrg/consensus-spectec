0a495eda84b413cf86893558fdea444769031906
GyeongMinDan <albert1121@skku.edu>
2025-11-20 23:11:38 +0900

    tmp cache.ml

--- spectec/lib/runtime-dynamic/cache.ml
+++ spectec/lib/runtime-dynamic/cache.ml
@@ -26,5 +26,44 @@ end
 
 let is_cached_func = function
   | name when String.starts_with ~prefix:"debug_print_label_" name -> false
+  (* State-modifying functions: do not cache (side-effects) *)
+  | "apply_one_round_captured" -> false
+  | "apply_one_round" -> false
+  | "apply_one_delta" -> false
+  | "increase_balance" -> false
+  | "decrease_balance" -> false
+  | "apply_one_decrease" -> false
+  | "reward_proposer" -> false
+  | "initiate_validator_exit" -> false
+  (* Functions that read/derive from state.BALANCES: do not cache *)
+  | "rebalance_validator_enumerated" -> false
+  | "expected_withdrawals_loop" -> false
+  | "get_expected_withdrawals" -> false
+  | "flag_reward_one" -> false
+  | "flag_reward_one_eligible" -> false
+  | "flag_penalty_one" -> false
+  | "flag_penalty_one_eligible" -> false
+  | "inact_penalty_one" -> false
+  | "get_flag_index_deltas" -> false
+  | "get_inactivity_penalty_deltas" -> false
+  | "get_total_balance" -> false
+  | "get_base_reward_per_increment" -> false
+  | "get_base_reward" -> false
+  | "compute_slash_penalty_en" -> false
   | _ -> true
-let is_cached_rule = function _ -> true
+let is_cached_rule = function
+  (* Relations that update state.BALANCES: do not cache *)
+  | "Process_rewards_and_penalties" -> false
+  | "Process_slashings" -> false
+  | "ProcessProposerSlashing" -> false
+  | "ProcessAttesterSlashing" -> false
+  | "ProcessAttestation" -> false
+  | "ProcessDeposit" -> false
+  | "Process_withdrawals" -> false
+  | "Process_sync_aggregate" -> false
+  (* Apply relations that call balance-updating rules: do not cache *)
+  | "Apply_proposer_slashings" -> false
+  | "Apply_attester_slashings" -> false
+  | "Apply_attestations" -> false
+  | "Apply_deposits" -> false
+  | _ -> true
